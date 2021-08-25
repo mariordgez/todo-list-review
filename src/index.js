42a6c065e44c658b507e759722827d7ed348cd99
mariordgez <mariordgez@hotmail.com>
2021-08-25 07:46:21 -0700

    commit for review

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import './style.css';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+import Task from './task.js';
+import Storage from './storage.js';
+import UI from './ui.js';
+
+const todoInput = document.querySelector('.todo-input');
+const todoAdd = document.querySelector('.todo-add');
+const todoClear = document.querySelector('.todo-clear');
+
+document.addEventListener(
+  'DOMContentLoaded',
+  UI.displayList(Storage.getList().list),
+);
+document.addEventListener('DOMContentLoaded', UI.buttonTask());
+
+const todo = (event) => {
+  event.preventDefault();
+  const index = Storage.getList().list.length + 1;
+  const newTask = new Task(todoInput.value, index);
+  Storage.saveTask(newTask);
+
+  UI.addToUI(newTask);
+  UI.checkTask();
+  UI.buttonTask();
+};
+
+const todoC = (event) => {
+  event.preventDefault();
+  Storage.deleteCompleted();
+  window.location.reload();
+};
+
+todoAdd.addEventListener('click', todo);
+todoClear.addEventListener('click', todoC);
